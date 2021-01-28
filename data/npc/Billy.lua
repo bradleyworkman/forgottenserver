4ff0185045f7e15e4bd92afc7eeee8b296eebec3
Bradley Workman <bworkman@codefox.us>
2021-01-27 20:57:50 -0500

    add NPC Billy to Rookgaard

--- data/npc/Billy.lua
+++ data/npc/Billy.lua
@@ -1,6 +1,6 @@
-require 'data/npc/lib/NPC'
+require 'data/npc/lib/ShopKeeper'
 
-local billy = NPC({'hi','hello'}, {{'Howdy |PLAYERNAME|.'},{'Howdy |PLAYERNAME|.','YOU RUDE $§&$'}}, nil)
+local billy = ShopKeeper({'hi','hello'}, 'Howdy |PLAYERNAME|.', 'YOU RUDE $§&$')
 
 function onCreatureAppear(...)      billy:onCreatureAppear(...)      end
 function onCreatureDisappear(...)   billy:onCreatureDisappear(...)   end
@@ -17,15 +17,12 @@ engine.all.connect('help', engine.State('Can\'t {help} you, sorry. I\'m a {cook}
 engine.all.connect('job', engine.State('I am farmer and a {cook}.'))
 engine.all.connect('time', engine.State('I came here to have some peace and leisure so leave me alone with \'time\'.'))
 engine.all.connect('king', engine.State('The {king} and his tax collectors are far away. You\'ll meet them soon enough.'))
-engine.all.connect({'spell','magic'}, engine.State('I can {spell} but know {no} {spell}.'))
+engine.all.connect({'spell','magic'}, engine.State('I can {spell} but know no {spell}.'))
 engine.all.connect('sell', engine.State('I {sell} various kinds of {food}.'))
 engine.all.connect('buy', engine.State('I {buy} {food} of most kind. Since I am a great {cook} I need much of it.'))
 engine.all.connect('food', engine.State('Are you looking for {food}? I have bread, cheese, ham, and meat.'))
 engine.all.connect('weapon', engine.State('Ask one of the shopkeepers. They make a fortune here with all those wannabe heroes.'))
 engine.all.connect('dungeon', engine.State('You\'ll find a lot of {dungeon}s if you look around.'))
-engine.all.connect('rat', engine.State('So you bring me a fresh {rat} for my famous stew?'))
-engine.all.connect('no', engine.State({{'Then not.'},{'$&*@!'}}))
-engine.all.connect('pan', engine.State('Have you found a {pan} for me?'))
 engine.all.connect('monsters', engine.State('Don\'t be afraid, in the town you should be save.'))
 engine.all.connect('amber', engine.State('Shes pretty indeed! I wonder if she likes bearded men.'))
 engine.all.connect('cipfried', engine.State('He never leaves this temple and only has {time} to care about those new arivals.'))
@@ -34,5 +31,48 @@ engine.all.connect('obi', engine.State('I like him, we usualy have a drink or tw
 engine.all.connect('seymour', engine.State('I don\'t like his headmaster behaviour. Then again, he IS a headmaster after all.'))
 engine.all.connect('willie', engine.State('Don\'t listen to that old wannabe, I\'m the best {cook} around.'))
 engine.all.connect('cook', engine.State('I am the best {cook} around. You can {sell} me most types of {food}.'))
+
+rat_state = engine.State('So you bring me a fresh {rat} for my famous stew?')
+
+engine.all.connect('rat', rat_state)
+
+rat_reward_state = engine.State()
+rat_reward_state.on_enter = function(player, query)
+    if player:removeItem(2813, 1) then
+        player:addMoney(2)
+    else
+        -- TODO figure out what Billy actually said if you didnt have a rat!
+        engine.respond(player, 'Hey! You don\'t have one!')
+    end
+end
+engine.all.from(rat_reward_state)
+
+rat_state.on_exit = function(player, query, destination)
+    if destination ~= rat_reward_state then
+        engine.respond(player, 'Then not.')
+    end
+end
+rat_state.to('yes', rat_reward_state)
+
+quest_state = engine.State('Have you found a pan for me?')
+engine.all.connect('pan', quest_state)
+
+reward_state = engine.State()
+engine.all.from(reward_state)
+reward_state.on_enter = function(player, query)
+    if player:removeItem(2563, 1) then
+        player:addItem(8704, 1)
+        engine.respond(player, 'A pan! At last! Take this in case you eat something my cousin has cooked.')
+    else
+        engine.respond(player, 'Hey! You don\'t have one!')
+    end
+end
+
+quest_state.to('yes', reward_state)
+quest_state.on_exit = function(player, query, destination)
+    if destination ~= reward_state then
+        engine.respond(player, '$&*@!')
+    end
+end
 
 engine.all.to('bye', engine.State(nil))
